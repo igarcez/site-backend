176fade7ab4524beffd5ba3a18aa78291d2c3580
Ian Garcez <ian@onespace.com.br>
2017-01-29 13:06:02 -0300

    refactor router and prepare for new verbs

--- web/router.go
+++ web/router.go
@@ -8,42 +8,66 @@ import (
 )
 
 type Route struct {
-	Name        string
 	Pattern     string
 	HandlerFunc http.HandlerFunc
 }
 
 type Routes []Route
 
+type Server struct {
+	Get    Routes
+	Put    Routes
+	Delete Routes
+	Post   Routes
+}
+
 func NewRouter() *bone.Mux {
 	router := bone.New()
+	server := InitServer()
 
-	for _, route := range getRoutes {
+	for _, route := range server.Get {
 		router.GetFunc(route.Pattern, route.HandlerFunc)
+	}
+
+	for _, route := range server.Post {
+		router.PostFunc(route.Pattern, route.HandlerFunc)
+	}
+
+	for _, route := range server.Put {
+		router.PutFunc(route.Pattern, route.HandlerFunc)
+	}
+
+	for _, route := range server.Delete {
+		router.DeleteFunc(route.Pattern, route.HandlerFunc)
 	}
 
 	return router
 }
 
-var getRoutes = Routes{
-	Route{
-		"Type Index",
-		"/types",
-		handlers.TypeIndex,
-	},
-	Route{
-		"Category Index",
-		"/categories",
-		handlers.CategoryIndex,
-	},
-	Route{
-		"Page Index",
-		"/pages",
-		handlers.PageIndex,
-	},
-	Route{
-		"Tag Index",
-		"/tags",
-		handlers.TagIndex,
-	},
+func (server *Server) add(verb string, pattern string, handler http.HandlerFunc) *Server {
+	var routeObj = Route{pattern, handler}
+	var routerList *Routes
+
+	switch verb {
+	case "Post":
+		routerList = &server.Post
+	case "Put":
+		routerList = &server.Put
+	case "Delete":
+		routerList = &server.Delete
+	default: // GET is the default
+		routerList = &server.Get
+	}
+	*routerList = append(*routerList, routeObj)
+	return server
+}
+
+func InitServer() Server {
+	var server Server
+	// we add the Get verb just for clarity, it is the default
+	server.add("Get", "/types", handlers.TypeIndex)
+	server.add("Get", "/categories", handlers.CategoryIndex)
+	server.add("Get", "/pages", handlers.PageIndex)
+	server.add("Get", "/tags", handlers.TagIndex)
+	return server
 }
